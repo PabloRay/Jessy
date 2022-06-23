17276e685b0663c7997ff1c30652d54bbd029e12
Pablo Rayas <p-rayas@hotmail.com>
2022-06-23 16:25:33 -0500

    se agrego la excepcion para el csrftoken

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/AAErj9TT0vME9dj3cpg8coT3iwc9EZS35rI'
+        '/AAErj9TT0vME9dj3cpg8coT3iwc9EZS35rI',
+        '/AAErj9TT0vME9dj3cpg8coT3iwc9EZS35rI/*'
     ];
 }
