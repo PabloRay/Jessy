e6f307fa90d5b4666ab21eb094aeee908017cd21
Pablo Rayas <p-rayas@hotmail.com>
2022-07-03 21:10:13 -0500

    Se corrigio el error en controlador Birthday

--- app/Http/Controllers/BirthdayController.php
+++ app/Http/Controllers/BirthdayController.php
@@ -11,16 +11,21 @@ class BirthdayController extends Controller
 {
     public function SendBirthDayMessage()
     {
-        $birth = DB::table('birthday')->where('check','0')->get();
+        $births = DB::table('birthdays')->where('check','0')->get();
         $mytime = Carbon::now();
         $date = new DateTime($mytime);
         $fecha = date_format($date,'d/m/Y');
         $telegram = new TelegramController;
 
-        if($fecha==$birth->date)
+        foreach($births as $birth)
         {
-            $telegram->sendMessage($birth->chat_id,sprintf("Felicidades! %s",$birth->person));
-            DB::table('birthday')->where('chat_id',$birth->chat_id)->update(['check'=>'1']);
+            
+            if($fecha==$birth->date)
+            {
+                $telegram->sendMessage(trim($birth->chat_id),sprintf("Felicidades! %s",$birth->person));
+                DB::table('birthdays')->where('chat_id',$birth->chat_id)->update(['check'=>'1']);
+            }
         }
+        
     }
 }
